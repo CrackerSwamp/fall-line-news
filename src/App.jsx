6a9c5c6b48561f1afccbbe7269d3b9bc461a198d
Ron Joiner <ronaldjoiner1@gmail.com>
2026-05-30 11:50:28 -0400

    Initial Fall Line site

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        background: "black",
+        minHeight: "100vh",
+        margin: 0,
+        overflow: "hidden",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <img
+        src="/falllinewide.png"
+        alt="Fall Line News"
+        style={{
+          width: "100%",
+          height: "100vh",
+          objectFit: "cover"
+        }}
+      />
+    </div>
+  )
+}
